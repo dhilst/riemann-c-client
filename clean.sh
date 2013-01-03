e1fa2992fab53b4654d105b504ee3b89e5fd1cf9
Daniel Hilst <danielhilst@gmail.com>
2013-01-03 19:31:00 -0200

    Minor fixes on build process

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-rm -rf Makefile.in aclocal.m4 autom4te.cache/ config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh missing src/Makefile.in src/examples/Makefile.in
+if test -f Makefile; then
+    make distclean
+fi
+rm -rf Makefile.in aclocal.m4 autom4te.cache/ config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh missing src/Makefile.in src/examples/Makefile.in m4 libriemann_client-*.tar.gz
