db0de34605b58c6e2d83b3a81b5518995a252545
Daniel Hilst Selli <danielhilst@gmail.com>
2016-10-02 18:59:31 -0300

    Relicensed with Apache License Version 2.0

--- src/riemann/udp.h
+++ src/riemann/udp.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2016, Daniel Hilst Selli <danielhilst@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 #ifndef RIEMANN_UDP_H
 # define RIEMANN_UDP_H
 
